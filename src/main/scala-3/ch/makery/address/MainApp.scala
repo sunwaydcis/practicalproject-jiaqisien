e08b89991161a350d2b858489583ab62e2b4849c
jiaqisien <jiaqi0878@gmail.com>
2025-06-10 22:07:38 +0800

    Learn new code
    FXML

---
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -0,0 +1,48 @@
+package ch.makery.address
+
+import javafx.fxml.FXMLLoader
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.Includes.*
+import javafx.scene as jfxs
+
+object MainApp extends JFXApp3:
+
+  //Window Root Pane
+  var roots: Option[scalafx.scene.layout.BorderPane] = None
+
+  override def start(): Unit =
+    // transform path of RootLayout.fxml to URI for resource location.
+    val rootResource = getClass.getResource("view/RootLayout.fxml")
+    // initialize the loader object.
+    val loader = new FXMLLoader(rootResource)
+    // Load root layout from fxml file.
+    loader.load()
+
+    // retrieve the root component BorderPane from the FXML
+    //convert javafx to scala fx (line13)
+    roots = Option(loader.getRoot[jfxs.layout.BorderPane])
+
+    stage = new PrimaryStage():
+      title = "AddressApp"
+      scene = new Scene():
+        //. get retrieve value from the option box
+        root = roots.get
+
+        //Once initialize, border pane come with menu bar
+
+    // call to display PersonOverview anchor pane when app start
+    showPersonOverview()
+  // actions for display person overview window
+  def showPersonOverview(): Unit =
+    val resource = getClass.getResource("view/PersonOverview.fxml")
+    val loader = new FXMLLoader(resource)
+    loader.load()
+    val roots = loader.getRoot[jfxs.layout.AnchorPane]
+    //retrieve value from the option object //javafx convert automatically to scalafx
+    this.roots.get.center = roots
+
+
+
+
